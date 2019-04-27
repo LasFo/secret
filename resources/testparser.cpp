ec8728678cf88bf7d2e747285d9d0d09849009af
Lasse Folger <lfolger@linux.fritz.box>
2019-04-27 20:03:23 +0200

    finished encoding +added classes for screen and choice

--- resources/testparser.cpp
+++ resources/testparser.cpp
@@ -8,14 +8,13 @@ int main(){
     ifs.open("text_data", std::ifstream::in);
     std::string s;
     getline(ifs, s);
-    size_t off = 0, end = 0;
     std::string tmp;
-    while(s.find("<<<", off) != std::string::npos)
-    {
-        end = s.find("<<<", off); 
-        tmp = s.substr(off, end - off);
-        off = end + 3;
-        std::cout << tmp << std::endl;
+    while(getline(ifs,s)) {
+        size_t off = 0, end = 0;
+        while(s.find("<<<", off) != std::string::npos) {
+            end = s.find("<<<", off); 
+            tmp = s.substr(off, end - off);
+            off = end + 3;
+        }
     }
-
 }
